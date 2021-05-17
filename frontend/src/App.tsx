0a63edc60e0286d9a48bbb02559f86db2ce5c23b
Kamil Nguyen Van <nvkga1@gmail.com>
2021-05-17 23:34:26 +0200

    feat: remove comments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -24,9 +24,6 @@ export default function App() {
                         </li>
                     </ul>
                 </nav>
-
-                {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
                 <Switch>
                     <Route path="/about">
                         <About />
