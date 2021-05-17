d78d697d4e5ef101b3a720afa14a00eb1c6c6bc1
Kamil Nguyen Van <nvkga1@gmail.com>
2021-05-17 23:09:22 +0200

    chore: add data

--- backend/src/store/mocked-dataset.ts
+++ backend/src/store/mocked-dataset.ts
@@ -1,10 +1,32 @@
 export const rooms = [{
     roomId: '-M_dwDw56FoZYjgdKLLl',
+    roomTitle: 'Monday catch up',
     messages: [{
         text: 'Hello'
     }, {
         text: 'Hi!'
     }, {
         text: 'Should we finish the task we started on Friday?'
+    }]
+}, {
+    roomId: '-M_gdKLLldwDw56FoZYj',
+    roomTitle: 'chess',
+    messages: [{
+        text: 'I challenge you'
+    }, {
+        text: 'which modality?'
+    }, {
+        text: 'Rapid chess is fine'
+    }, {
+        text: 'It will be easy'
+    }]
+}, {
+    roomId: '-M_wDw23FogdKLLldZYj',
+    messages: []
+}, {
+    roomId: '-M_6FogdKLLlwDw5dZYj',
+    roomTitle: 'sprint planning',
+    messages: [{
+        text: 'Good morning team!'
     }]
 }]
